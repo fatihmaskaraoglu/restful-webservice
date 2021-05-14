dcc63bea832d5338be2a4888479acfa365f8acdd
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2021-05-14 20:17:15 +0300

    Return correct HTTP Status Code, Location, Exception
    Enhancing POST Method to return correct HTTP
    Status Code and Location
    Implementing Exception Handling - 404 Resource
    Not Found

--- src/main/java/com/fatihmas/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/fatihmas/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.fatihmas.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,13 +18,17 @@ public class UserResource {
     }
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user==null){
+            throw new UserNotFoundException("id-"+id);
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
-
-
 }
