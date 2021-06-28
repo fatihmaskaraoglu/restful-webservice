dccd84e836d8b77d7f04a6055943f88688a9fab2
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2021-06-28 17:10:48 +0300

    Versioning RESTful Services - Basic Approach with URIs
    Versioning RESTful Services - Basic Approach with URIs

---
+++ src/main/java/com/fatihmas/rest/webservices/restfulwebservices/version/PersonV2.java
@@ -0,0 +1,17 @@
+package com.fatihmas.rest.webservices.restfulwebservices.version;
+
+public class PersonV2 {
+    private Name name;
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+}
